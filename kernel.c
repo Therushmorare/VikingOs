91f0cfe65c8f5e2b81984d78597ffb63296d0b0a
therushmorare <tshego.tt.morare@gmail.com>
2025-05-31 03:22:59 +0200

    kernal.c patch

--- kernel.c
+++ kernel.c
@@ -1,6 +1,5 @@
 // kernel.c
 void main() {
-    char* video = (char*) 0xb8000;
-    video[0] = 'K';
-    video[1] = 0x07; // White on black
+    char* video_memory = (char*) 0xb8000;
+    *video_memory = 'X';
 }
